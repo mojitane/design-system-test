8be792d3baf4afd6d23c1b418cf777a242a08427
Maximilian Henrich <maximilian.henrich@ibmix.de>
2025-12-16 01:10:00 +0100

    feat: Fix entry point

--- packages/input/tsup.config.ts
+++ packages/input/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
+  entryPoints: ["src/input.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   external: ["react"],
